0e048c51326bc00759892a709521c4af99090844
AMAL SEBASTIAN <64699226+amal404@users.noreply.github.com>
2021-06-22 14:26:48 +0530

    Weather capture python program and its output

---
+++ wt.py
@@ -0,0 +1,43 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+
+#f = open("C:/Users/Amal Sebastian/desktop/wt.txt", mode='r', encoding='utf-8')
+
+with open("wt.txt",'w',encoding = 'utf-8') as f:
+   f.write("-------------------------------------------------------------\n")
+   f.write("Weather Stats for - {}  || {}\n".format(location.upper(), date_time))
+   f.write("-------------------------------------------------------------\n")
+   f.write("Current temperature is: {:.2f} deg C\n".format(temp_city))
+   f.write("Current weather desc  :")
+   f.write(weather_desc)
+   f.write("\nCurrent Humidity      :")
+   f.write(str(hmdt))
+   f.write('%\n')
+   f.write("Current wind speed    :")
+   f.write(str(wind_spd))
+   f.write('kmph')
+   print("\nWrite compeleted:)")
